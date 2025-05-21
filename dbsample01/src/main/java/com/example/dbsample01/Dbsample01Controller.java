41dc2ba74e3cb35bc0724cd67bfca6f9e2fe1ed6
cr1teacher <ikeda_toshiaki@edu.kimura.ac.jp>
2025-05-21 10:40:33 +0900

    250521

--- dbsample01/src/main/java/com/example/dbsample01/Dbsample01Controller.java
+++ dbsample01/src/main/java/com/example/dbsample01/Dbsample01Controller.java
@@ -3,6 +3,7 @@ package com.example.dbsample01;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,23 +16,30 @@ public class Dbsample01Controller {
     @Autowired
     UserRepository repos;
     
+    // @RequestMapping(value="/", method=RequestMethod.GET)
+    // public ModelAndView index(@ModelAttribute("formModel") User user, ModelAndView mav) {
+    //     mav.setViewName("index");
+    //     Iterable<User> list = repos.findAll();
+    //     mav.addObject("data", list);
+    //     return mav;
+    // }
     @RequestMapping(value="/", method=RequestMethod.GET)
-    public ModelAndView index(
-            @ModelAttribute("formModel") User user,
-            ModelAndView mav) {
-        mav.setViewName("index");
-        Iterable<User> list = repos.findAll();
-        mav.addObject("data", list);
-        return mav;
+    public String index(@ModelAttribute("formModel") User user, Model model) {
+        model.addAttribute("data", repos.findAll());
+        return "index";
     }
 
     @RequestMapping(value="/", method=RequestMethod.POST)
     @Transactional(readOnly=false)
-    public ModelAndView form(
-            @ModelAttribute("formModel") User user,
-            ModelAndView mav) {
+    // public ModelAndView form(
+    //         @ModelAttribute("formModel") User user,
+    //         ModelAndView mav) {
+    //     repos.saveAndFlush(user);
+    //     return new ModelAndView("redirect:/");
+    // }
+    public String form(@ModelAttribute("formModel") User user, Model model) {
         repos.saveAndFlush(user);
-        return new ModelAndView("redirect:/");
+        return "redirect:/";
     }
     
     @PostConstruct
